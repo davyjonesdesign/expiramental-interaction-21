215b3584920afa818f0b17d675c3c7ffa5a4bd33
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-06-27 21:08:33 -0500

    added POC project 2

---
+++ project2/poc/script.js
@@ -0,0 +1,43 @@
+const Component1 = {
+    template: '#landing',
+
+};
+const Component2 = {
+    template: '#boaters'
+};
+const Component3 = {
+    template: '#dolphins'
+};
+const Component4 = {
+    template: '#sharks'
+};
+
+Vue.component('landing', {
+  template: '#landing'
+})
+
+const routes = [
+    { path: '', component: Component1 },
+    { path: '/boaters', component: Component2 },
+    { path: '/dolphins', component: Component3 },
+    { path: '/sharks', component: Component4 }
+
+]
+
+const router = new VueRouter({
+    routes
+});
+
+const eventBus = new Vue({});
+
+new Vue({
+  data: {
+    direction: 'slide-right'
+  },
+  created() {
+    eventBus.$on('change-direction', (newDirection) => {
+      this.direction = newDirection;
+    });
+  },
+  router
+}).$mount('#app');
